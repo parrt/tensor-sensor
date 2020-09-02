d19337df22624d9c4cd02e69a5bbf9fb0f481291
parrt <parrt@cs.usfca.edu>
2020-09-02 13:06:57 -0700

    add notes

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     author='Terence Parr',
     author_email='parrt@cs.usfca.edu',
     python_requires='>=3.6',
-#    install_requires=['graphviz'], # needs numpy if you use ndarrayviz()
-    description='',
+    install_requires=['graphviz','numpy','pytorch'],
+    description='The goal of this library is to generate more helpful exception messages for numpy/pytorch matrix algebra expressions.',
 #    keywords='visualization data structures',
     classifiers=['License :: OSI Approved :: MIT License',
                  'Intended Audience :: Developers']
