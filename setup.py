2834dfed292c1180035f61c621ccb3dc7315cd01
parrt <parrt@cs.usfca.edu>
2020-09-02 13:25:46 -0700

    tweak

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='matricks',
+    name='tensor-sensor',
     version='0.1',
-    url='https://github.com/parrt/matricks',
+    url='https://github.com/parrt/tensor-sensor',
     license='MIT',
-    py_modules=['matricks'],
+    py_modules=['tsensor'],
     author='Terence Parr',
     author_email='parrt@cs.usfca.edu',
     python_requires='>=3.6',
     install_requires=['graphviz','numpy','pytorch'],
-    description='The goal of this library is to generate more helpful exception messages for numpy/pytorch matrix algebra expressions.',
+    description='The goal of this library is to generate more helpful exception messages for numpy/pytorch tensor algebra expressions.',
 #    keywords='visualization data structures',
     classifiers=['License :: OSI Approved :: MIT License',
                  'Intended Audience :: Developers']
