1b353ae8bffd014ad8ea0e0231e5dc491a533841
parrt <parrt@cs.usfca.edu>
2020-09-21 09:40:31 -0700

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tensor-sensor',
-    version='0.1a8',
+    version='0.1a9',
     url='https://github.com/parrt/tensor-sensor',
     license='MIT',
     py_modules=['tsensor.parsing', 'tsensor.ast', 'tsensor.analysis', 'tsensor.viz'],
