e0c70f9603c8b5539d1507d60c89188beb25d5ea
parrt <parrt@cs.usfca.edu>
2020-11-25 17:03:07 -0800

    tweak test

--- testing/ones.py
+++ testing/ones.py
@@ -1,14 +1,8 @@
+# Regression test for https://github.com/parrt/tensor-sensor/issues/16
+# Should not throw exception or error, just show equation.
 import numpy as np
 import tsensor
-
-def foo():
-   x = 3**3
-   bar()
-
-def bar():
-   print("foo")
 
 print(tsensor.__version__)
 with tsensor.explain():
    a = np.ones(3)
-   # foo()
