3f2ad74ef5d1ffb977d0fce65212de5795b8dc87
Simon Brugman <sbrugman@users.noreply.github.com>
2021-09-27 18:37:36 +0200

    feat: dtypes option

--- tsensor/analysis.py
+++ tsensor/analysis.py
@@ -48,7 +48,7 @@ class clarify:
                  vectorcolor="#fefecd", char_sep_scale=1.8, fontcolor='#444443',
                  underline_color='#C2C2C2', ignored_color='#B4B4B4', error_op_color='#A40227',
                  show:(None,'viz')='viz',
-                 hush_errors=True):
+                 hush_errors=True, dtypes=False):
         """
         Augment tensor-related exceptions generated from numpy, pytorch, and tensorflow.
         Also display a visual representation of the offending Python line that
@@ -109,15 +109,16 @@ class clarify:
                             unhandled code caught by my parser are ignored. Turn this off
                             to see what the error messages are coming from my parser.
         :param show: Show visualization upon tensor error if show='viz'.
+        :param dtypes: Display the matrix elementwise type below the vector/matrix (default: False).
         """
-        self.show = show
-        self.fontname, self.fontsize, self.dimfontname, self.dimfontsize, \
+        self.show, self.fontname, self.fontsize, self.dimfontname, self.dimfontsize, \
         self.matrixcolor, self.vectorcolor, self.char_sep_scale,\
         self.fontcolor, self.underline_color, self.ignored_color, \
-        self.error_op_color, self.hush_errors = \
-            fontname, fontsize, dimfontname, dimfontsize, \
+        self.error_op_color, self.hush_errors, self.dtypes = \
+            show, fontname, fontsize, dimfontname, dimfontsize, \
             matrixcolor, vectorcolor, char_sep_scale, \
-            fontcolor, underline_color, ignored_color, error_op_color, hush_errors
+            fontcolor, underline_color, ignored_color, error_op_color, hush_errors, \
+            dtypes
 
     def __enter__(self):
         self.frame = sys._getframe().f_back # where do we start tracking? Hmm...not sure we use this
@@ -146,7 +147,8 @@ class clarify:
                                               self.char_sep_scale, self.fontcolor,
                                               self.underline_color, self.ignored_color,
                                               self.error_op_color,
-                                              hush_errors=self.hush_errors)
+                                              hush_errors=self.hush_errors,
+                                              dtypes=self.dtypes)
                 if self.view is not None: # Ignore if we can't process code causing exception (I use a subparser)
                     if self.show=='viz':
                         self.view.show()
@@ -159,8 +161,7 @@ class explain:
                  dimfontname='Arial', dimfontsize=9, matrixcolor="#cfe2d4",
                  vectorcolor="#fefecd", char_sep_scale=1.8, fontcolor='#444443',
                  underline_color='#C2C2C2', ignored_color='#B4B4B4', error_op_color='#A40227',
-                 savefig=None,
-                 hush_errors=True):
+                 savefig=None, hush_errors=True, dtypes=False):
         """
         As the Python virtual machine executes lines of code, generate a
         visualization for tensor-related expressions using from numpy, pytorch,
@@ -229,15 +230,16 @@ class explain:
                             to see what the error messages are coming from my parser.
         :param savefig: A string indicating where to save the visualization; don't save
                         a file if None.
+        :param dtypes: Display the matrix elementwise type below the vector/matrix (default: False).
         """
-        self.savefig = savefig
-        self.fontname, self.fontsize, self.dimfontname, self.dimfontsize, \
+        self.savefig, self.fontname, self.fontsize, self.dimfontname, self.dimfontsize, \
         self.matrixcolor, self.vectorcolor, self.char_sep_scale,\
         self.fontcolor, self.underline_color, self.ignored_color, \
-        self.error_op_color, self.hush_errors = \
-            fontname, fontsize, dimfontname, dimfontsize, \
+        self.error_op_color, self.hush_errors, self.dtypes = \
+            savefig, fontname, fontsize, dimfontname, dimfontsize, \
             matrixcolor, vectorcolor, char_sep_scale, \
-            fontcolor, underline_color, ignored_color, error_op_color, hush_errors
+            fontcolor, underline_color, ignored_color, error_op_color, hush_errors, \
+            dtypes
 
     def __enter__(self):
         # print("ON trace", sys._getframe())
@@ -301,6 +303,7 @@ class ExplainTensorTracer:
         filename, line = info.filename, info.lineno
         name = info.function
 
+        # Note: always true since L292 above...
         if event=='line':
             self.line_listener(module, name, filename, line, info, frame)
 
@@ -338,7 +341,8 @@ class ExplainTensorTracer:
                                  self.explainer.char_sep_scale, self.explainer.fontcolor,
                                  self.explainer.underline_color, self.explainer.ignored_color,
                                  self.explainer.error_op_color,
-                                 hush_errors=self.explainer.hush_errors)
+                                 hush_errors=self.explainer.hush_errors,
+                                 dtypes=self.explainer.dtypes)
         self.views.append(view)
         if self.explainer.savefig is not None:
             file_path = Path(self.explainer.savefig)
@@ -493,6 +497,15 @@ def _smallest_matrix_subexpr(t, nodes) -> bool:
 
 def istensor(x):
     return _shape(x) is not None
+
+
+def _dtype(v) -> str:
+    if hasattr(v, "dtype"):
+        if v.dtype.__class__.__module__ == "torch":
+            # ugly but works
+            return str(v.dtype).replace("torch.", "")
+        return v.dtype.name
+    return None
 
 
 def _shape(v):
